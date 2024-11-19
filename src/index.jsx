6d9b4a07ec2c9501849b34d32ef23d5bfabafc0d
jbrjteb1234 <james@bright.me.uk>
2024-11-19 23:16:10 +0000

    Fixed vite build environment

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'features/app.jsx';
+import App from './features/app.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App /> 
