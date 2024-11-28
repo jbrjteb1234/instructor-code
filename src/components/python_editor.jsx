860da0786b33b07f1ea374fd53abfc975fa39d13
jbrjteb1234 <james@bright.me.uk>
2024-11-28 03:44:06 +0000

    Scaffold code for react editor functionality

---
+++ src/components/python_editor.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function python_editor() {
+    const [code, set_code] = useState('');
+
+    function handle_code_run() {
+
+    }
+
+    return (
+        <div className="editor">
+            <h2>Python Editor</h2>
+        </div>
+    );
+
+}
+
+export default Editor;
