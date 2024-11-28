65c82b558f226ea0cb0a4c25c1fdaaf15c0c98ab
jbrjteb1234 <james@bright.me.uk>
2024-11-28 11:46:23 +0000

    text areas to update use states for translator and editor

--- src/components/nl_translator.jsx
+++ src/components/nl_translator.jsx
@@ -9,13 +9,17 @@ function NLTranslator( {code} ) {
         
     }
 
-    function handleSetCodeInput(){
-
+    function handleSetCodeInput(e){
+        setInput(e.target.value);
     }
 
     return (
         <div className="nl-translator">
             <h2>Natural Language Translator</h2>
+            <textarea
+                value={input}
+                onChange={handleSetCodeInput}
+            />
         </div>
     );
 }
--- src/components/python_editor.jsx
+++ src/components/python_editor.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
 
-function PythonEditor( {code, set_Code} ) {
+function PythonEditor( {code, setCode} ) {
 
     const [output, setOutput] = useState('');
 
-    function handleExecuteCode() {
-
+    function handleCodeChange(e) {
+        setCode(e.target.value);
     }
 
     return (
         <div className="editor">
             <h2>Python Editor</h2>
+            <textarea
+                value={code}
+                onChange={handleCodeChange}
+            />
         </div>
     );
 
