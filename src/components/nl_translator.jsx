295118ba28bb9e9e0c1351e61445708a8f44c1eb
jbrjteb1234 <james@bright.me.uk>
2024-11-28 03:48:24 +0000

    Scaffold code for nl_translator

---
+++ src/components/nl_translator.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function NLTranslator() {
+    const [input, set_input] = useState('');
+    const [translated_code, set_translated_code] = useState('');
+
+    function handle_set_translated_code() {
+        
+    }
+
+    function handle_set_input(){
+
+    }
+
+    return (
+        <div className="nl-translator">
+            <h2>Natural Language Translator</h2>
+        </div>
+    );
+}
+
+export default NLTranslator;
