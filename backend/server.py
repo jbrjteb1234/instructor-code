de08a2465808351e60249ccba0c3f3c06f69b0f7
jbrjteb1234 <james@bright.me.uk>
2024-12-08 03:03:05 +0000

    Set up translator server entry point in flask

--- backend/server.py
+++ backend/server.py
@@ -1,2 +1,17 @@
 from flask import Flask, request, jsonify
-from transformers import pipeline
+from flask_cors import CORS
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+import torch
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/translate', methods=['POST'])
+def translate(): 
+
+        response = "Server reached"
+
+        return {"response": response}
+
+if __name__ == "__main__":
+    app.run(debug=True)
