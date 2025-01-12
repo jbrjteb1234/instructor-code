c35d420baba3bce2831b6efdeb0fb1ed9d50b0fd
jbrjteb1234 <james@bright.me.uk>
2025-01-12 18:14:56 +0000

    Added backend handling for query function

--- backend/server.py
+++ backend/server.py
@@ -10,6 +10,21 @@ CORS(app)
 load_dotenv()
 client = OpenAI()
 
+def complete(userPrompt, systemPrompt):
+    completion = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": systemPrompt},
+            {"role": "user", "content": userPrompt}
+        ],
+        model="gpt-4o-mini", 
+        max_tokens=1024,
+        temperature=0.7
+    )
+
+    output = completion.choices[0].message.content
+
+    return output
+
 @app.route('/prompt-assistance', methods=['POST'])
 def prompt():
 
@@ -20,28 +35,19 @@ def prompt():
         userPrompt = f"User's question: {inputPrompt}\nUser's code: {code}"
         systemPrompt = "Provide assistance to the user. The user is a beginner learning python, answer their promopt in a pedagogical and instructive manner, teachning not doing. Provide limited python code to assist with this. Only address glaring blatant issues with the code, don't suggest potential improvements. Only produce an answer which directly addresses the user's prompt and code, no introductions or anything of the sort. Address a maximum of TWO issues in the code"
 
-        completion = client.chat.completions.create(
-            messages=[
-                {"role": "system", "content": systemPrompt},
-                {"role": "user", "content": userPrompt}
-            ],
-            model="gpt-4o-mini", 
-            max_tokens=1024,
-            temperature=0.7
-        )
-
-        output = completion.choices[0].message.content
-
-        return output
+        return complete(userPrompt, systemPrompt)
 
 @app.route('/query-error', methods=['POST'])
 def error():
 
-        data = request.json
+        payload = request.json
+        error = payload.get('prompt','')
+        code = payload.get('code','')
 
-        response = "Server reached"
+        userPrompt = f"User's error after running: {error}\nUser's code: {code}"
+        systemPrompt = "Provide assistance to the user. The user is a beginner learning python and has encountered an error. In a pedagogical and instructive manner, provide limited python code to assist with this. Only address glaring blatant issues with the code, don't suggest potential improvements. Only produce an answer which directly addresses the user's error and code, no introductions or anything of the sort. Address ONLY the issue with the code which is causing the error"
 
-        return {"response": response}
+        return complete(userPrompt, systemPrompt)
 
 if __name__ == "__main__":
     app.run(debug=True)
